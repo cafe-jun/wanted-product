54024fb89cf57ab506f218f5b0fb001f13bfed08
cafe_jun <tb25271@gmail.com>
2024-05-23 17:34:20 +0900

    [feat] project setting

---
+++ src/main/java/com/example/cafejun/CafejunApplication.java
@@ -0,0 +1,13 @@
+package com.example.cafejun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafejunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafejunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cafejun/CafejunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cafejun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafejunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
